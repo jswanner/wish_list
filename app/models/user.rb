198027bce6ed8df515dcf7f6f32f00ae4ed3c4b2
Jacob Swanner <swanner@gmail.com>
2009-12-02 21:16:43 -0500

    removing need for email

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ActiveRecord::Base
   has_many  :attendings
   has_many  :events,    :through => :attendings
 
-  validates_presence_of   :username,  :email
-  validates_uniqueness_of :username,  :email
+  validates_presence_of   :username #,  :email
+  validates_uniqueness_of :username #,  :email
 
   attr_accessible :username, :email
 
--- db/migrate/20091127201429_create_users.rb
+++ db/migrate/20091127201429_create_users.rb
@@ -7,12 +7,12 @@ class CreateUsers < ActiveRecord::Migration
       t.timestamps
     end
     add_index :users, :username,  :unique => true
-    add_index :users, :email,     :unique => true
+    #add_index :users, :email,     :unique => true
   end
 
   def self.down
     remove_index :users, :username
-    remove_index :users, :email
+    #remove_index :users, :email
     drop_table :users
   end
 end

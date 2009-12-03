03e547ed6b944d002a937b6374e6681e35ae7b3c
Jacob Swanner <swanner@gmail.com>
2009-12-03 00:39:07 -0500

    now showing the wishes count along side username on users/index

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+  def username_with_meta(user)
+    sprintf(
+      "%s %s",
+      user.username,
+      content_tag(
+        :span,
+        "( #{ pluralize user.wishes_count, 'wish' } )",
+        :class => 'meta'
+      )
+    )
+  end
 end

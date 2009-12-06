495749a7f0c7f31991915ba676ef009c5fdd1b2a
Jacob Swanner <swanner@gmail.com>
2009-12-06 00:03:48 -0500

    added a menu bar at the top and some color to the site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def title(title)
     content_for(:title) { title }
   end
+
+  def header_menu(item)
+    content_for(:header_menu) do
+      content_tag :li, item
+    end
+  end
 end
